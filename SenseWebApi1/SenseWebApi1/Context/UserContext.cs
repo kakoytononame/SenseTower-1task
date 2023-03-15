6ff50896991ce1c567c42ce0ef42add1c9954aee
kakoytononame <95782287+kakoytononame@users.noreply.github.com>
2023-03-15 22:26:52 +0300

    addnewusers

--- SenseWebApi1/SenseWebApi1/Context/UserContext.cs
+++ SenseWebApi1/SenseWebApi1/Context/UserContext.cs
@@ -1,7 +1,26 @@
-﻿namespace SenseWebApi1.Context
+﻿using SenseWebApi1.domain.Entities;
+
+namespace SenseWebApi1.Context
 {
     public class UserContext
     {
+        List<User> Users = new List<User>();
+
+        public UserContext() 
+        {
+            Users.Add(
+                new User()
+                {
+                    UserId= Guid.Parse("d04a76aa-2f20-4af9-a39e-427623323374"),
+                    UserName="User 1"
+                });
+            Users.Add(
+                new User()
+                {
+                    UserId = Guid.Parse("da2a7240-8fbb-48eb-bdcb-cc154593587f"),
+                    UserName = "User 1"
+                });
+        }
 
     }
 }
